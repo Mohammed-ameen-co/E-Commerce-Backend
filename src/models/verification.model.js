9584788d62f1206a7162e6a53247d37cf7c6d92f
Mohammed-ameen-co <aameenrangrej4@gmail.com>
2026-03-02 14:04:27 +0530

    Created New Email Verification Models

---
+++ src/models/verification.model.js
@@ -0,0 +1,42 @@
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+
+const verificationSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: [true, "User ID is required"],
+      index: true,
+    },
+    token: {
+      type: String,
+      required: [true, "Token is required"],
+      
+    },
+    expiresAt: {
+      type: Date,
+      required: [true, "Expiration time is required"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+verificationSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
+verificationSchema.pre("save", async function () {
+  if (!this.isModified("token")) {
+    return ;
+  }
+  this.token = crypto.createHash("sha256").update(this.token).digest("hex");
+  return ;
+});
+verificationSchema.methods.compareToken = async function (token) {
+ const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
+ return this.token === hashedToken;
+};
+const verificationModel = mongoose.model("verification", verificationSchema);
+
+module.exports = verificationModel;
