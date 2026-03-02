112ce57a9d7b9e0d6b00f506ef203afd39ed977b
Mohammed-ameen-co <aameenrangrej4@gmail.com>
2026-03-02 13:54:11 +0530

    Created new Admin controller

---
+++ src/controllers/admin.controller.js
@@ -0,0 +1,25 @@
+const userModel = require('../models/user.model');
+
+async function handleCreateAdmin(req,res){
+    const isAdminExists = await userModel.findOne({role:"admin"});
+    if(!isAdminExists){
+        await userModel.create({
+            name:"Admin",
+            email:process.env.ADMIN_EMAIL,
+            password:process.env.ADMIN_PASSWORD,
+            role:"admin",
+            isEmailVerified:true
+        })
+        return res.status(201).json({
+            success:true,
+            message:"Admin user created successfully"
+        })
+    }
+    return res.status(400).json({
+        message:"Admin is already exists"
+    })
+}
+
+module.exports = {
+    handleCreateAdmin
+}
