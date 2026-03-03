dd47768e9107de49818463e2c5315a91982b5b95
Mohammed-ameen-co <aameenrangrej4@gmail.com>
2026-03-03 15:40:37 +0530

    Formating

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,11 +3,12 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controllers/user.controller");
+const middleware = require("../middleware/auth.middleware");
 
 router.post("/signup", userController.handleUserRegister);
 router.post("/login", userController.handleUserLogin);
 
-router.post("/logout", userController.handleLogoutUsers);
+router.post("/logout", middleware.authMiddleware, userController.handleLogoutUsers);
 
 router.post("/refresh-token", userController.handleRefreshTokenUsers);
 
