a5e75e4a66eb6bc024a2df19e8106d07e2466cee
Mohammed-ameen-co <aameenrangrej4@gmail.com>
2026-03-03 15:30:55 +0530

    I have added try catch block.

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,25 +1,33 @@
-const userModel = require('../models/user.model');
+const userModel = require("../models/user.model");
 
-async function handleCreateAdmin(req,res){
-    const isAdminExists = await userModel.findOne({role:"admin"});
-    if(!isAdminExists){
-        await userModel.create({
-            name:"Admin",
-            email:process.env.ADMIN_EMAIL,
-            password:process.env.ADMIN_PASSWORD,
-            role:"admin",
-            isEmailVerified:true
-        })
-        return res.status(201).json({
-            success:true,
-            message:"Admin user created successfully"
-        })
+async function handleCreateAdmin(req, res) {
+  try {
+    const isAdminExists = await userModel.findOne({ role: "admin" });
+    if (!isAdminExists) {
+      await userModel.create({
+        name: "Admin",
+        email: process.env.ADMIN_EMAIL,
+        password: process.env.ADMIN_PASSWORD,
+        role: "admin",
+        isEmailVerified: true,
+      });
+      return res.status(201).json({
+        success: true,
+        message: "Admin user created successfully",
+      });
     }
     return res.status(400).json({
-        message:"Admin is already exists"
-    })
+      message: "Admin is already exists",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error creating admin user",
+      error: error.message,
+    });
+  }
 }
 
 module.exports = {
-    handleCreateAdmin
-}
+  handleCreateAdmin,
+};
