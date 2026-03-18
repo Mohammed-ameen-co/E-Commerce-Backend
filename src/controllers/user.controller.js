2412ab598e9fa6b2246af6d91cba641fc8f293f5
Mohammed-ameen-co <aameenrangrej4@gmail.com>
2026-03-18 14:21:58 +0530

    Added 3 new function and fix bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -31,10 +31,11 @@ async function handleUserRegister(req, res) {
     await verificationModel.create({
       userId: user._id,
       token: verificationToken,
+      type: "EMAIL_VERIFY",
       expiresAt: new Date(Date.now() + 10 * 60 * 1000), // 10 minutes
     });
 
-    const verificationLink = `${process.env.BASE_URL}/api/verify-email/${verificationToken}`;
+    const verificationLink = `${process.env.BASE_URL}/api/verify-email/${verificationToken}`;//If you are using the frontend, replace BASE_URL with CLIENT_URL.
     await emailUtils.sendSignUpEmail(user.email, user.name, verificationLink);
 
     return res.status(201).json({
@@ -71,7 +72,7 @@ async function handleUserLogin(req, res) {
         .status(403)
         .json({ message: "Please verify your email before logging in" });
     }
-    const isPasswrodValid = await user.comparePassword(password, user.password);
+    const isPasswrodValid = await user.comparePassword(password);
 
     if (!isPasswrodValid) {
       return res
@@ -197,9 +198,170 @@ async function handleRefreshTokenUsers(req, res) {
   }
 }
 
+//this is change current password
+async function changePassword(req, res) {
+  try {
+    const { password, newPassword, conffPassword } = req.body;
+    const user = req.user;
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found during auth" });
+    }
+
+    if (!password || !newPassword || !conffPassword) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (newPassword !== conffPassword) {
+      return res.status(400).json({ message: "Password do not match" });
+    }
+
+    if (newPassword.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+    const getUser = await userModel.findById(user._id).select("+password");
+
+    const isMatch = await getUser.comparePassword(password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Current password is incorrect" });
+    }
+
+    getUser.password = newPassword;
+
+    await getUser.save();
+
+    await sessionModel.deleteMany({ userId: user._id });
+
+    return res.status(200).json({ message: "password change" });
+  } catch (error) {
+    return res.status(500).json({
+      message: "internal server error",
+      error: error.message,
+    });
+  }
+}
+
+
+//this is send forgot password link
+async function forgotPassword(req, res) {
+  try {
+    const { email } = req.body;
+
+    if (!email) {
+      return res.status(400).json({
+        message: "Email required",
+      });
+    }
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(200)
+        .json({ message: "if this email exists, a reset link hase been sent" });
+    }
+
+    await verificationModel.deleteMany({
+      userId: user._id,
+      type: "RESET_PASSWORD",
+    });
+    const verificationToken = crypto.randomBytes(32).toString("hex");
+    await verificationModel.create({
+      userId: user._id,
+      token: verificationToken,
+      type: "RESET_PASSWORD",
+      expiresAt: new Date(Date.now() + 10 * 60 * 1000), // 10 minutes
+    });
+
+    const resetLink = `${process.env.BASE_URL}/api/reset-password/${user._id}/${verificationToken}`;//If you are using the frontend, replace BASE_URL with CLIENT_URL.
+    await emailUtils.sendForgotPasswordEmail(user.email, user.name, resetLink);
+
+    return res.status(200).json({
+      message: "if this email exists, a reset link hase been sent",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "internal server error",
+      error: error.message,
+    });
+  }
+}
+
+//this is reset current password
+async function resetPassword(req, res) {
+  try {
+    const { userId, token } = req.params;
+    const { newPassword, confirmPassword } = req.body;
+
+    if (!userId || !token) {
+      return res.status(400).json({ message: "Invalid request" });
+    }
+    if (!newPassword || !confirmPassword) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (newPassword !== confirmPassword) {
+      return res.status(400).json({ message: "Password do not match" });
+    }
+
+    if (newPassword.length < 6) {
+      return res.status(400).json({
+        message: "Password must be at least 6 characters",
+      });
+    }
+
+    const verification = await verificationModel.findOne({
+      userId,
+      type: "RESET_PASSWORD",
+    });
+
+    if (!verification) {
+      return res.status(400).json({
+        message: "Invalid or expired token",
+      });
+    }
+    const isMatch = await verification.compareToken(token);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid or expired token",
+      });
+    }
+
+    const user = await userModel.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    user.password = newPassword;
+    await user.save();
+
+    await verificationModel.deleteOne({ _id: verification._id });
+
+    await sessionModel.deleteMany({ userId });
+
+    return res.status(200).json({
+      message: "Successfully set new password",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+}
+
 module.exports = {
   handleUserRegister,
   handleUserLogin,
   handleLogoutUsers,
   handleRefreshTokenUsers,
+  changePassword,
+  forgotPassword,
+  resetPassword,
 };
